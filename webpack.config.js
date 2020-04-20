a8e70aa2a8121c6c8f699d7ce85c41fd60f16d24
Vivian <vlshibukawa@gmail.com>
2020-04-20 16:42:59 -0400

    step 8

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,14 @@ module.exports = {
   devServer: {
     contentBase: "./dist",
   },
+  module: {
+    rules:[
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    ]
+  },
+  resolve:{
+    extensions: ['.js', '.jsx']
+  }
 };
